1e848cc36fa0ec21e36cb3aa3a90ec911bc070c9
fhogan <fhogan@mit.com>
2018-01-16 18:30:48 -0500

    New gripper implementatino

--- catkin_ws/src/apc_planning/src/gs_flip_publisher.py
+++ catkin_ws/src/apc_planning/src/gs_flip_publisher.py
@@ -7,19 +7,20 @@ import sensor_msgs.msg
 import cv2
 from cv_bridge import CvBridge
 
-def flip_image(data):
+def flip_image(data, topic):
     #flip image using opencv
     bridge = CvBridge()
     cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
     flip_cv_image = cv2.flip(cv_image,1)
     flip_cv_image = cv2.flip(flip_cv_image,0)
     #publish flipped image
-    flip_image_pub = rospy.Publisher("rpi/gelsight/flip_raw_image",sensor_msgs.msg.Image, queue_size = 10)
+    flip_image_pub = rospy.Publisher(topic,sensor_msgs.msg.Image, queue_size = 10)
     flip_image_pub.publish(bridge.cv2_to_imgmsg(flip_cv_image, "bgr8"))
 
 
 def listener():
-    image_sub = rospy.Subscriber("rpi/gelsight/raw_image", sensor_msgs.msg.Image, flip_image)
+    image_sub = rospy.Subscriber("rpi/gelsight/raw_image", sensor_msgs.msg.Image, flip_image, "rpi/gelsight/flip_raw_image")
+    image_sub2 = rospy.Subscriber("rpi/gelsight/raw_image2", sensor_msgs.msg.Image, flip_image, "rpi/gelsight/flip_raw_image2")
     rospy.spin()
 
 if __name__ == '__main__':
